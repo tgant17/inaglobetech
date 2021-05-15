41819fb81de1727b5f559ed26dc93d09cd15ff82
tgant17 <44172930+tgant17@users.noreply.github.com>
2021-05-14 22:44:27 -0500

    eecs647

--- src/app.js
+++ src/app.js
@@ -3,7 +3,10 @@ const express = require('express')
 const ejs = require('ejs')
 const bodyParser = require('body-parser')
 
-const insertName = require('./utils/connect.js');
+const sqlConnect = require('./utils/connect.js');
+const insertPrj = sqlConnect.insertPrj;
+const insertOrg = sqlConnect.insertOrg;
+const displayProject = sqlConnect.displayProject; 
 
 app = express();
 
@@ -19,17 +22,66 @@ app.use(express.static(publicDirectoryPath));
 //create application / json parser 
 var urlEncodedParser = bodyParser.urlencoded({ extended : false }); 
 
+app.use(urlEncodedParser); 
+
+//home page kinda (for our project)
 app.get('', (req, res) => {
     res.render('index', {qs: req.query});
 })
 
+// path /projectSubmission will render the projectSubmission page 
+app.get('/projectSubmission', (req, res) => {
+    res.render('projectSubmission');
+})
 
+// path /projectSubmission will render the projectSubmission page 
+app.get('/organizationRegistration', (req, res) => {
+    res.render('organizationRegistration');
+})
+
+//this will be the page to view projects
+app.get('/projects', (req, res) => {
+    
+    displayProject((error, data) => {
+        if(error) 
+        {
+            return res.send({error}) 
+        }
+        else 
+        {
+            console.log(data.rows)
+            res.render('projects', {row : data.rows})
+        }
+    })
+})
+
+//submits the project submission form and inserts into the 
+//Project table
 // takes the form and passes the requests body
-app.post('', urlEncodedParser, (req, res) => {
+/*
+    *projectName
+    *duration
+    *description
+    *attempts
+    *email 
+    *consequences
+    *magicWand
+*/ 
+app.post('/projectSubmission', urlEncodedParser, (req, res) => {
     
     res.render('name-success', {data: req.body});
     data = req.body; 
-    insertName(data.first, data.last);
+    console.log(data)
+    insertPrj(data.projectName, data.description, data.duration, data.email, data.consequences, data.attempts, data.magicWand);
+})
+
+
+app.post('/organizationRegistration', urlEncodedParser, (req, res) => {
+    
+    res.render('name-success', {data: req.body});
+    data = req.body; 
+    console.log(data)
+    insertOrg(data.name, data.address, data.headperson, data.phoneNum, data.email);
 })
 
 
--- src/utils/connect.js
+++ src/utils/connect.js
@@ -33,20 +33,107 @@ app.get('/', function(req, resp) {
     });
 })
 
+//helper function that makes sure each ID is unique for it's respective table
+//takes in a table name as it's param 
+//runs a sql query to find the greatest id value in the table and adds one
+//THIS CAN BE USED AS ONE OF OUR QUERIES FOR THE PROJECT (maybe?)
+const uniqueID = (tableName, id, callback) => {
+
+    //variable to hold the query
+    findMaxQuery = "SELECT MAX(" + id + ") AS p FROM " + tableName
+
+    //connect to the database and find the largest ID for the given table
+    connection.query(findMaxQuery, (error, rows, fields) =>{
+        if(error)
+            callback('error', undefined, undefined); 
+        else
+            callback(undefined, (Number(rows[0].p + 1))); 
+    })
+
+}
+
 
 // takes in params from form and sets value in SQL query / inserts
 const insertProject = (name, description, duration, email, consequences, attempts, magicWand) => {
 
     //create function to find ID
+    //placeholder 
+    var projectID = 0
+    var tableName = 'project'
+    var idName = 'projectid'
 
-    connection.query("INSERT INTO PROJECTS VALUES (" + "'" + first + "'" + ', ' + "'" + last + "'" + ');', function(error, rows, fields){
-        if(error)
-            console.log(error);
+    uniqueID(tableName, idName, (error, newID) => {
+        if(error) 
+            console.log(error); 
         else 
-            console.log('successfully added ' + first + ' ' + last + ' to the names table');
+        {
+            projectID = newID; 
+            insertQuery = "INSERT INTO PROJECT(NAME, PROJECTID, DESCRIPTION, EMAIL, CONSEQUENCES, ATTEMPTS, MAGICWAND, DURATIONOFPROBLEM) VALUES (" + "'" + name + "'" + ', ' + "'" + projectID + "'" +"," +"'"+description+"'"+","+"'"+email+"'"+","+"'"+consequences+"'"+","+"'"+attempts+"'"+","+"'"+magicWand+"'"+","+"'"+duration+"'"+");"
+            
+            connection.query(insertQuery, function(error, rows, fields){
+                if(error)
+                    console.log(error);
+                else 
+                    console.log('successfully added \n\tProject:', name, '\n\tProject ID:', projectID);
+            })
+        }
+    }); 
+}
+
+// takes in params from form and sets value in SQL query / inserts into the organization table
+// 
+const insertOrg = (name, address, headperson, phoneNum, email) => {
+
+    //variable declaration 
+    var orgId = 0
+    var tableName = 'organization' 
+    var idName = 'orgid'
+
+    uniqueID(tableName, idName, (error, newID) => {
+        if(error) 
+            console.log(error); 
+        else 
+        {
+            projectID = newID
+            insertQuery = "INSERT INTO ORGANIZATION(NAME, ADDRESS, HEADPERSON, ORGID, PHONENUMBER, EMAIL) VALUES (" + "'" + name + "'" + "," + "'" + address + "'" + "," + "'" + headperson + "'" + "," + "'" + projectID + "'" + "," + "'" + phoneNum + "'" + "," +"'" + email + "'" + ");"
+
+            connection.query(insertQuery, function(error, rows, field){
+                if(error)
+                    console.log(error); 
+                else 
+                    console.log('successfully added \n\tOrganization:', name, '\n\tProjectID:', projectID); 
+            })
+        }
     })
 }
 
-module.exports = insertProject; 
+//gets project information from datbase and displays it
+const displayProject = (callback) => {
+
+    getProjectsQuery = 'SELECT DISTINCT NAME, DESCRIPTION, CONSEQUENCES, DURATIONOFPROBLEM, EMAIL FROM PROJECT WHERE STATUS IS NULL';
+
+    connection.query(getProjectsQuery, function(error, rows, field){
+        if(error) 
+            callback('error', undefined)
+        else 
+            console.log(rows)
+            callback(undefined, { rows : rows})
+    })
+
+}
+
+
+// app.get('*', (req, res) => {
+//     res.render('404', {
+//         title: '404', 
+//         errorMsg: 'Page not found',
+//         name: 'tristan'
+//     })
+// })
+
+
+exports.insertPrj = insertProject; 
+exports.insertOrg = insertOrg; 
+exports.displayProject = displayProject; 
 
 // app.listen(1337); 
